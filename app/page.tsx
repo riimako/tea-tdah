f64c5f31189440c0030d51023ace3d6b8d2ff4d1
Riina Maria Korpela <riina.korpela@guidesmiths.com>
2022-12-03 13:54:55 +0100

    fix: home layout css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import styles from '../styles/home.module.scss';
 
 export default function Page() {
 
-  return (<>
+  return (
+  <>
     <Link href="/lets-start">
       <h1 className={styles.title}>
         ¡Bienvenidos!
@@ -13,26 +14,22 @@ export default function Page() {
     <div className={styles.grid}>
       <Link href="/whats-wrong-with-me" className={styles.card}>
         <h2>¿Depresión, ansiedad, o qué me pasa?</h2>
-        <p>Find in-depth information about Next.js features and API.</p>
+        <p>La importancia del autoconocimiento</p>
+      </Link>
+
+      <Link href="/who-i-am" className={styles.card}>
+        <h2>¿Cómo aprovechar tus singularidades?</h2>
+        <p>Mi superpoder vs mi kryptonita</p>
       </Link>
 
       <Link href="/what-is-normal" className={styles.card}>
         <h2>¿Qué c*j*nes es la normalidad?</h2>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
+        <p>La normalidad como horizonte</p>
       </Link>
-
-
-      <Link href="/who-i-am" className={styles.card}>
-        <h2>¿Cómo aprovechar tus singularidades?</h2>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </Link>
-
 
       <Link href="/self-reflection" className={styles.card}>
         <h2>Auto reflexión</h2>
-        <p>
-          Instantly deploy your Next.js site to a public URL with Vercel.
-        </p>
+        <p>Bienvenidos al temporal</p>
       </Link>
     </div>
   </>)
