a241f72bf4d40bb7c9c4f7a72d37e2148d6e6062
Riina Maria Korpela <riina.korpela@guidesmiths.com>
2022-12-05 16:10:03 +0100

    stategies page

--- app/whats-wrong-with-me/strategies/page.tsx
+++ app/whats-wrong-with-me/strategies/page.tsx
@@ -4,13 +4,16 @@ export default function Page() {
     <p>Son esfuerzos y conductas que realizamos para hacer frente a demandas, y a conflictos entre ellos.
         Todos tenemos predisposición a confiar en los estrategias negativas.
     </p>
-    <h4>Negativos</h4>
+    <h4>Negativos más comunes</h4>
     <ul>
         <li>Evitar conflictos</li>
+        <li>Catastrofizar</li>
         <li>Dormir excesivamente</li>
         <li>Abuso de sustancias</li>
         <li>Compras impulsivas</li>
         <li>Comer poco/demasiado</li>
+        <li>Romantizar el pasado</li>
+        <li>Respuestas físicas o anímicas exageras</li>
     </ul>
 
     </>
