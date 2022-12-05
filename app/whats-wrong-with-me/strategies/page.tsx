1d15eb1fd5440635b4f14c34b3196563d7fc808c
Riina Maria Korpela <riina.korpela@guidesmiths.com>
2022-12-05 16:08:06 +0100

    stategies page

--- app/whats-wrong-with-me/strategies/page.tsx
+++ app/whats-wrong-with-me/strategies/page.tsx
@@ -1,33 +1,18 @@
-import Image from 'next/image';
-import styles from '../../styles/general.module.scss';
 export default function Page() {
     return (
-    <>
-       has desarrollado unhealthy coping strategies?
-to avoid showing whats truly going inside you?
+    <><h2>¿Qué son las estrategias de afrontamiento?</h2>
+    <p>Son esfuerzos y conductas que realizamos para hacer frente a demandas, y a conflictos entre ellos.
+        Todos tenemos predisposición a confiar en los estrategias negativas.
+    </p>
+    <h4>Negativos</h4>
+    <ul>
+        <li>Evitar conflictos</li>
+        <li>Dormir excesivamente</li>
+        <li>Abuso de sustancias</li>
+        <li>Compras impulsivas</li>
+        <li>Comer poco/demasiado</li>
+    </ul>
 
-coping strategies-->estrategias de AFRONTAMIENTO
-unhealthy mas comunes
-avoiding issues
-sleeping too much
-alcohol o uso de drogas
-impulsive spending
-over or under eating
-
-we are all predisposed to relying on negative coping skills
-
-saludable?
-exercise
-tomarse un baño
-hobby,
-amigos o preguntar por ayuda
-afrontar las problemas
-
-me dijo la psicologa una vez
-
-"si no estás dispuesta a actuar por ello
-por que cuesta o porque no quieres,
-debes dejar de darle importancia"
     </>
     )
 }
